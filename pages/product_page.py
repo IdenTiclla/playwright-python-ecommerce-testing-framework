d7b34bba53af250cb94538e0a485d03c66915d13
IdenTiclla <iden.ticlla@gmail.com>
2025-08-30 20:24:47 -0400

    Add quantity input handling to ProductPage and corresponding test
    - Introduced a new locator for the quantity input field in the ProductPage class to facilitate quantity management.
    - Added a method to retrieve the current product quantity, enhancing the functionality of the product page.
    - Implemented a test case in TestProduct to verify the default quantity displayed on the product page, ensuring accurate assertions for user interactions.
    
    This commit improves the product page's usability by allowing quantity adjustments and validating the default state in tests.

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,5 @@
 from pages.base_page import BasePage
 
-
 class ProductPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
@@ -11,6 +10,7 @@ class ProductPage(BasePage):
         self.product_price = page.locator("div.price h3")
         self.product_availability = page.locator("span[class='badge badge-danger'], span[class='badge badge-success']")
         self.decrease_quantity_button = page.locator("div[class='entry-row row order-3 no-gutters '] button[aria-label='Decrease quantity']")
+        self.quantity_input = page.locator("div[class='entry-row row order-3 no-gutters '] input[name='quantity']")
         self.increase_quantity_button = page.locator("div[class='entry-row row order-3 no-gutters '] button[aria-label='Increase quantity']")
         self.compare_this_product_button = page.locator("div[data-id='216844'] button[title='Compare this Product']")
         self.size_chart_button = page.locator("div[class='entry-row row order-10 order-sm-8 order-md-7 '] a").nth(1)
@@ -20,3 +20,7 @@ class ProductPage(BasePage):
 
     def get_product_availability(self):
         return self.product_availability.text_content()
+    
+    def get_product_quantity(self):
+        """Get the product quantity"""
+        return int(self.quantity_input.input_value())
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,4 +1,5 @@
 from tests.base_test import BaseTest
+from playwright.sync_api import expect
 
 
 class TestProduct(BaseTest):
@@ -19,3 +20,9 @@ class TestProduct(BaseTest):
         self.home_page.carousel.navigate_to_next_slide()
         self.home_page.carousel.slides.nth(2).click()
         assert self.product_page.get_product_availability() == "In Stock"
+
+    def test_default_quantity_on_product_page(self):
+        self.home_page.goto()
+        self.home_page.carousel.slides.nth(0).click()
+        assert self.product_page.get_product_quantity() == 1
+        
