356bd221a468910e645ce4daa6f3416002eba2c6
IdenTiclla <iden.ticlla@gmail.com>
2025-09-02 19:15:49 -0400

    Add product addition to cart functionality and corresponding test
    - Introduced methods in the ProductPage class to handle adding products to the cart, including filling the quantity and clicking the add button.
    - Added a new test case in TestProduct to verify the addition of a product to the cart, ensuring the correct notification message and cart count updates.
    
    This commit enhances the product page's usability by allowing users to add products to their cart and validating this functionality through tests.

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,9 +1,12 @@
 from pages.base_page import BasePage
-
+from components.notification import Notification
+from components.header_actions import HeaderActions
 class ProductPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
         self.page = page
+        self.notification = Notification(page)
+        self.header_actions = HeaderActions(page)
 
         self.wishlist_button = page.locator("btn btn-wishlist wishlist-28").first
         self.product_name = page.locator("div[class*='content-title'] h1")
@@ -12,6 +15,9 @@ class ProductPage(BasePage):
         self.decrease_quantity_button = page.locator("div[class='entry-row row order-3 no-gutters '] button[aria-label='Decrease quantity']")
         self.quantity_input = page.locator("div[class='entry-row row order-3 no-gutters '] input[name='quantity']")
         self.increase_quantity_button = page.locator("div[class='entry-row row order-3 no-gutters '] button[aria-label='Increase quantity']")
+        
+        self.add_to_cart_button = page.locator("//div[@class='entry-row row order-3 no-gutters ']//button[normalize-space(text()) = 'Add to Cart']")
+        
         self.compare_this_product_button = page.locator("div[data-id='216844'] button[title='Compare this Product']")
         self.size_chart_button = page.locator("div[class='entry-row row order-10 order-sm-8 order-md-7 '] a").nth(1)
         self.popup_button = page.locator("div[class='entry-row row order-10 order-sm-8 order-md-7 '] a").nth(2)
@@ -40,3 +46,13 @@ class ProductPage(BasePage):
     def decrease_product_quantity(self):
         """Decrease the product quantity"""
         self.decrease_quantity_button.click()
+
+    def fill_product_quantity(self, quantity):
+        """Fill the product quantity"""
+        self.quantity_input.clear()
+        self.quantity_input.fill(str(quantity))
+
+    def add_product_to_cart(self, quantity=1):
+        """Add the product to the cart"""
+        self.fill_product_quantity(quantity)
+        self.add_to_cart_button.click()
