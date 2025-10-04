b4a87cd9cc2897b63ab40ebce9164bbfb322c0d7
IdenTiclla <iden.ticlla@gmail.com>
2025-10-03 20:39:38 -0400

    feat: add author href retrieval methods in ArticlePage and AuthorContent components
    - Implemented a method in AuthorContent to retrieve the author's href attribute.
    - Added a corresponding method in ArticlePage for accessing the author's href.
    - Updated tests to verify that the author hrefs are consistent across components.

--- components/author_content.py
+++ components/author_content.py
@@ -7,13 +7,17 @@ class AuthorContent(BaseComponent):
         super().__init__(page)
         self.page = page
         self.author_image = page.locator("div[class*='content-author '] a[class*='flex-shrink']")
-        self.author_name = page.locator("div[class*='content-author '] h5")
+        self.author_name = page.locator("div[class*='content-author '] h5 a")
         self.author_description = page.locator("div[class*='content-author '] div[class*='author-description']")
 
 
     def get_author_name(self):
         """Get the author name"""
         return self.author_name.text_content()
+
+    def get_author_href(self):
+        """Get the author href attribute"""
+        return self.author_name.get_attribute("href")
     
     def get_author_description(self):
         """Get the author description"""
