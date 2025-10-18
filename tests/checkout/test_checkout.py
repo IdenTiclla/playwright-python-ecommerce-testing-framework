8e8e7b0b783ac9ed868499220a5943001fa82d69
IdenTiclla <iden.ticlla@gmail.com>
2025-10-18 11:01:01 -0400

    feat: add CheckoutPage class and corresponding test for direct checkout
    - Introduced a new CheckoutPage class to manage checkout functionalities.
    - Updated BaseTest to include an instance of CheckoutPage.
    - Added a test case for direct checkout with an available product for unlogged users, verifying cart count and page URL.

--- tests/checkout/test_checkout.py
+++ tests/checkout/test_checkout.py
@@ -22,4 +22,28 @@ class TestCheckout(BaseTest):
 
         # assert page url
         assert self.page.url == "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/cart"
+
+    def test_direct_checkout_with_available_product_unlogged_user(self):
+        # navigate to the home page
+        self.home_page.goto()
+
+        # scroll down to the top products section
+        self.home_page.top_products.scroll_to_top_products()
+
+        # add third product to the cart
+        self.home_page.top_products.add_product_to_cart(index=3)
+
+        # click on the checkout button
+        self.home_page.notification.click_on_checkout_button()
+
+        # verify the amount of products in the cart is 1
+
+        cart_count = self.checkout_page.header_actions.get_cart_count()
+        assert cart_count == "1"
+
+        # get page url
+        get_page_url = self.checkout_page.page.url
+
+        # verify page url
+        assert get_page_url == "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/checkout"
     
