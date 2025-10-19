07978532d35dfc8a97895942213bcd1e42db22ae
IdenTiclla <iden.ticlla@gmail.com>
2025-10-19 18:21:52 -0400

    feat: add radio button selection methods and tests in CheckoutPage
    - Introduced methods to check the selection state of login, register, and guest radio buttons in the CheckoutPage class.
    - Updated the checkout test to verify that the register radio is selected by default, while login and guest radios are not selected.

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -10,3 +10,21 @@ class CheckoutPage(BasePage):
 
         # Components
         self.header_actions = HeaderActions(page)
+
+        # Locators
+        self.login_radio = page.locator("#input-account-login")
+        self.register_radio = page.locator("#input-account-register")
+        self.guest_radio = page.locator("#input-account-guest")
+
+
+    def is_login_radio_selected(self):
+        """Check if the login radio is selected"""
+        return self.login_radio.is_checked()
+
+    def is_register_radio_selected(self):
+        """Check if the register radio is selected"""
+        return self.register_radio.is_checked()
+
+    def is_guest_radio_selected(self):
+        """Check if the guest radio is selected"""
+        return self.guest_radio.is_checked()
